358bb1bcf313ea7a1234fd77073c8da3c4e9662d
Samrat Bose <samratofficial2009@gmail.com>
2024-06-25 15:50:29 +0530

    working on backend

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+mongoose.connect('mongodb://localhost:27017/typing-speed-test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+
+app.use('/api/users', userRoutes);
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/template.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
