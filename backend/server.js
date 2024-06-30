6e2a3c5f4ab2bf978b185ce9ab6553bd07816ab9
Samrat Bose <samratofficial2009@gmail.com>
2024-06-30 12:50:05 +0530

    changes to backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -7,26 +7,24 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
-
 app.use(bodyParser.json());
 app.use(cors());
-
 
 mongoose.connect('mongodb://localhost:27017/typing-speed-test', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
+  .catch(err => {
+      console.error('MongoDB connection error:', err.message);
+      process.exit(1);
+  });
 
 app.use('/api/users', userRoutes);
 
-
-app.use(express.static(path.join(__dirname, '../public')));
-
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/template.html'));
+    res.sendFile(path.join(__dirname, '../frontend/public', 'template.html'));
 });
 
 const PORT = process.env.PORT || 5000;
